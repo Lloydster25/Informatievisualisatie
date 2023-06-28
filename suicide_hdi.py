0f22e651d3cd18ab8d0c693bdc1271420eb01534
Lloyd de Rouw <lloyd.derouw@hotmail.nl>
2023-06-28 15:08:18 +0200

    final notebook aangemaakt

--- suicide_hdi.py
+++ suicide_hdi.py
@@ -2,21 +2,7 @@ import pandas as pd
 import numpy as np
 
 df1 = pd.read_csv('databases/IV DATASET 1.csv')
-df2 = pd.read_csv('databases/IV DATASET 2.csv')
-merge = pd.merge(df1, df2, on='Country')
-merge.to_csv('databases/FINAL DATASET.csv')
 
+print(df1)
 
-
-merge['Suicide Ratio (M/F)'] = np.where(merge['suicides/100k female'] != 0,
-                                     merge['suicides/100k male'] / merge['suicides/100k female'],
-                                     np.nan)
-
-
-print(merge)
-
-# Calculate the mean of the 'Suicide Ratio (M/F)' column
-suicide_ratio_mean = merge['Suicide Ratio (M/F)'].mean()
-
-# Print the mean
-print("Mean Suicide Ratio (M/F):", suicide_ratio_mean)
+df1.to_csv('databases/FINAL DATASET.csv')
