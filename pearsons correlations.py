3766c261391a1ef4cd680a6aef52135755f44d78
Lloyd de Rouw <lloyd.derouw@hotmail.nl>
2023-06-29 19:31:46 +0200

    nieuwe database

--- pearsons correlations.py
+++ pearsons correlations.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import pearsonr, spearmanr
+from scipy.stats import pearsonr
 
-df = pd.read_csv('databases/FINAL DATASET.csv')
-print(df)
+# Read the dataset from the CSV file
+df = pd.read_csv('docs/databases/FINAL DATASET.csv')
 
 # Drop rows with infinite or NaN values from the selected columns
 df_cleaned = df.replace([np.inf, -np.inf], np.nan).dropna()
@@ -11,26 +11,13 @@ df_cleaned = df.replace([np.inf, -np.inf], np.nan).dropna()
 # Extract the cleaned columns
 a = df_cleaned['HDI 2015']
 b = df_cleaned['GDI 2015']
-c = df_cleaned['GII 2015']
-x = df_cleaned['Average suicide 2013']
-
+x = df_cleaned['Average suicide 2015']
 
 # Calculate correlation using Pearson's correlation coefficient
-hdi_gdi_p, _ = pearsonr(a,b)
-hdi_gii_p, _ = pearsonr(a,c)
-gdi_gii_p, _ = pearsonr(b,c)
-
-hdi_p, _ = pearsonr(a,x)
-gdi_p, _ = pearsonr(b,x)
-gii_p, _ = pearsonr(c,x)
-
-
+hdi_gdi_p, _ = pearsonr(a, b)
+hdi_suicide_p, _ = pearsonr(a, x)
+gdi_suicide_p, _ = pearsonr(b, x)
 
 print('HDI-GDI Pearson:', hdi_gdi_p)
-print('HDI-GII Pearson:', hdi_gii_p)
-print('GDI-GII Pearson:', gdi_gii_p)
-print('')
-print('HDI-SUICIDE Pearson:', hdi_p)
-print('GDI-SUICIDE Pearson:', gdi_p)
-print('GII-SUICIDE Pearson:', gii_p)
-
+print('HDI-Suicide Pearson:', hdi_suicide_p)
+print('GDI-Suicide Pearson:', gdi_suicide_p)
