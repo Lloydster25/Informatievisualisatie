3accc2085beda8eaf27209c6bce4e836b79da64f
Lloyd de Rouw <lloyd.derouw@hotmail.nl>
2023-06-29 16:00:40 +0200

    perspective 3 database

--- suicide_hdi.py
+++ suicide_hdi.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 
 df1 = pd.read_csv('databases/IV DATASET 1.csv')
-
-print(df1)
-
-df1.to_csv('databases/FINAL DATASET.csv')
+df2 = pd.read_csv('databases/IV DATASET 3.csv')
+df2 = df2.drop('Unnamed: 0', axis=1)
+print(df2)
+df = pd.merge(df1, df2, on='Country')
+print(df)
+df.to_csv('databases/FINAL DATASET.csv')
