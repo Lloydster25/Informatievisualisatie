ce07f1dd273f1de016d0b9f4adeb72a677d7293e
Lloyd de Rouw <lloyd.derouw@hotmail.nl>
2023-07-01 14:47:17 +0200

    visualisations helemaal af

--- suicide_hdi.py
+++ suicide_hdi.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df1 = pd.read_csv('docs/databases/IV DATASET 1.csv')
 df2 = pd.read_csv('docs/databases/IV DATASET 3.csv')
-df2 = df2.drop('Unnamed: 0', axis=1)
+
 print(df2)
 df = pd.merge(df1, df2, on='Country')
 print(df)
