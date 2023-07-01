a1cbec1d1712dbefecf8e6565ff0d062b857d504
Lloyd de Rouw <lloyd.derouw@hotmail.nl>
2023-07-01 15:57:29 +0200

    site.

---
+++ docs/Code/Final Merge.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('docs/databases/IV DATASET 1.csv')
+df2 = pd.read_csv('docs/databases/IV DATASET 3.csv')
+
+print(df2)
+df = pd.merge(df1, df2, on='Country')
+print(df)
+df.to_csv('docs/databases/FINAL DATASET.csv')
